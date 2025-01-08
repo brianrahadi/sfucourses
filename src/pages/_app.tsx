a40f80067e06e48213b4205504b058018cc884e1
Brian Rahadi <brian.rahadi@gmail.com>
2025-01-08 01:10:20 -0800

    feat: course page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,19 +5,21 @@ import "../styles/main.scss";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
+  const queryClient = new QueryClient();
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Helmet pageTitle={router.pathname} />
       <HeaderNav />
       <Component {...pageProps} key={router.asPath} />
       <Footer />
       <Analytics />
       <SpeedInsights />
-    </>
+    </QueryClientProvider>
   );
 };
 
