34f0343f74496a1503ae46120ef42d5f4c41de9c
Brian Rahadi <brian.rahadi@gmail.com>
2025-02-08 22:40:57 -0500

    fix: remove unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,2 @@
 export { useExploreFilters } from "./UseExploreFilters";
 export { useCourseOfferings } from "./UseCourseOfferings";
-export { useOutlinesWithSections } from "./UseOutlinesWithSections";
