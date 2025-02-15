db45768266340ce2feed98cb0e78b95cb34bb228
Brian Rahadi <brian.rahadi@gmail.com>
2025-02-15 15:03:54 -0500

    feat: refactor img, jpg to webp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { Hero } from "@components";
 import Link from "next/link";
-import LandingPageHeroSrc from "../assets/images/landing-page/hero.jpg";
+import LandingPageHeroSrc from "../assets/images/landing-page/hero.webp";
 
 interface LandingPageProps {}
 
