fcaaa07f3ff3eb8600ab0ade284a316176c4681f
Brian Rahadi <brian.rahadi@gmail.com>
2025-03-09 23:33:56 -0400

    fix: get time for position

--- src/components/WeeklySchedule.tsx
+++ src/components/WeeklySchedule.tsx
@@ -415,7 +415,7 @@ export const WeeklySchedule: React.FC<WeeklyScheduleProps> = ({
 
         // Get time with corrections for position
         // 1. Calculate Y position relative to grid
-        const relativeY = event.clientY - gridRect.top;
+        const relativeY = event.clientY - gridRect.top - 55;
 
         // 2. Adjust for header height
         const adjustedY = relativeY - headerHeight;
